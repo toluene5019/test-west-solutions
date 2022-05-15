2e2eb7b6f55d8a060fa0527b3b1e2111786bb714
toluene5019 <42148663+toluene5019@users.noreply.github.com>
2022-05-16 01:28:57 +0300

    wip sort by price and mileage

--- js/script.js
+++ js/script.js
@@ -26,62 +26,136 @@ menuItems.forEach(
 )
 //-----MOBILE MENU-----//
 
-const make = document.getElementById("car-make");
-const yearItems = document.querySelectorAll(".card__item");
+// const make = document.getElementById("car-make");
+// const yearItems = document.querySelectorAll(".card__item");
 
-make.addEventListener("change", function () {
-  let value = make.value;
-  [...yearItems].forEach((item) => {
-    if (value === "") {
-        item.classList.remove("hidden");
-    } else {
-        const make = item.dataset.make;
-        if (value === make) {
-            item.classList.remove('hidden')
-        } else {
-            item.classList.add('hidden')
-        }
-    }
-  });
-});
+// make.addEventListener("change", function () {
+//   let value = make.value;
+//   [...yearItems].forEach((item) => {
+//     if (value === "") {
+//         item.classList.remove("hidden");
+//     } else {
+//         const make = item.dataset.make;
+//         if (value === make) {
+//             item.classList.remove('hidden')
+//         } else {
+//             item.classList.add('hidden')
+//         }
+//     }
+//   });
+// });
 
 
-const year = document.getElementById("car-year");
-const items = document.querySelectorAll(".card__item");
+// const year = document.getElementById("car-year");
+// const items = document.querySelectorAll(".card__item");
 
-year.addEventListener("change", function () {
-  let value = year.value;
-  [...items].forEach((item) => {
-    if (value === "") {
-        item.classList.remove("hidden");
-    } else {
-        const year = item.dataset.year;
-        if (value === year) {
-            item.classList.remove('hidden')
-        } else {
-            item.classList.add('hidden')
-        }
-    }
-  });
-});
+// year.addEventListener("change", function () {
+//   let value = year.value;
+//   [...items].forEach((item) => {
+//     if (value === "") {
+//         item.classList.remove("hidden");
+//     } else {
+//         const year = item.dataset.year;
+//         if (value === year) {
+//             item.classList.remove('hidden')
+//         } else {
+//             item.classList.add('hidden')
+//         }
+//     }
+//   });
+// });
 
-const mileage = document.getElementById("car-mileage");
-const elements = document.querySelectorAll(".card__item");
+// const mileage = document.getElementById("car-mileage");
+// const elements = document.querySelectorAll(".card__item");
 
-mileage.addEventListener("change", function () {
-  let value = mileage.value;
+// mileage.addEventListener("change", function () {
+//   let value = mileage.value;
 
-  [...elements].forEach((element) => {
-    if (value === "") {
-      element.classList.remove("hidden");
-    } else {
-      const mileage = +element.dataset.mile;
-      if (mileage < value ) {
-        element.classList.remove("hidden");
-      } else {
-        element.classList.add("hidden");
+//   [...elements].forEach((element) => {
+//     if (value === "") {
+//       element.classList.remove("hidden");
+//     } else {
+//       const mileage = +element.dataset.mile;
+//       if (mileage < value ) {
+//         element.classList.remove("hidden");
+//       } else {
+//         element.classList.add("hidden");
+//       }
+//     }
+//   });
+// });
+
+
+// const sortType = document.getElementById("sort-type")
+// const sortElements = document.querySelectorAll(".card__item")
+
+// // console.log(sortElements);
+// sortType.addEventListener("change", function () {
+//   let value = sortType.value;
+
+//   [...sortElements].forEach((element) => {
+//     if (value === "mile-asc") {
+//       function comparator(a, b) {
+//         return b.dataset.mile - a.dataset.mile
+//         // if (a.dataset.mile < b.dataset.mile)
+//         //     return -1;
+//         // if (a.dataset.mile > b.dataset.mile)
+//         //     return 1;
+//         // return 0;
+//     }
+//   }
+//   });
+// });
+
+const sortType = document.getElementById("sort-type")
+const sort = document.querySelector(".offers__products");
+const sortItems = document.querySelectorAll(".card__item");
+// const button = document.querySelector('.offers__btn');
+
+sortType.addEventListener('change', function() {
+  let value = sortType.value;
+    if ( value === 'mile-asc') {
+      const sorted = [...sortItems].sort(function(a, b) {
+        return a.dataset.mile - b.dataset.mile;
+      });
+
+      sort.innerHTML = '';
+      for (let el of sorted) {
+        sort.appendChild(el)
       }
     }
-  });
-});
 
+    else if (value === 'mile-desc') {
+      const sorted = [...sortItems].sort(function(a, b) {
+        return b.dataset.mile - a.dataset.mile;
+      });
+
+      sort.innerHTML = '';
+      for (let el of sorted) {
+        sort.appendChild(el)
+      }
+    }
+
+    else if (value === 'price-asc') {
+      const sorted = [...sortItems].sort(function(a, b) {
+        return a.dataset.price - b.dataset.price;
+      });
+
+      sort.innerHTML = '';
+      for (let el of sorted) {
+        sort.appendChild(el)
+      }
+    }
+
+    else if (value === 'price-desc') {
+      const sorted = [...sortItems].sort(function(a, b) {
+        return b.dataset.price - a.dataset.price;
+      });
+
+      sort.innerHTML = '';
+      for (let el of sorted) {
+        sort.appendChild(el)
+      }
+    }
+
+})
